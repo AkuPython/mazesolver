dede5b5effd9f926e282d0e0a4b7ee0319e64079
AkuPython <12842971+AkuPython@users.noreply.github.com>
2025-03-03 20:52:25 +1030

    add lines

--- graphics.py
+++ graphics.py
@@ -24,4 +24,26 @@ class Window:
     def close(self):
         self.__running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color):
+        x1, y1 = self.point_a.x, self.point_a.y
+        x2, y2 = self.point_b.x, self.point_b.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
+
 
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+    tl = (2, 2)
+    tr = (798, 2)
+    bl = (2, 598)
+    br = (798, 598)
+    top = Line(Point(*tl), Point(*tr))
+    bottom = Line(Point(*bl), Point(*br))
+    left = Line(Point(*tl), Point(*bl))
+    right = Line(Point(*tr), Point(*br))
+
+    win.draw_line(top, "red")
+    win.draw_line(bottom, "black")
+    win.draw_line(left, "green")
+    win.draw_line(right, "blue")
     win.wait_for_close()
 
 
